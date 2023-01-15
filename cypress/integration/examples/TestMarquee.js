e09f59c13e69a92b2c1bb0caf63cb7176daef7d5
Sukhbir <sukhbirbrar83@gmail.com>
2023-01-14 18:30:38 -0800

    Test Marquee Changed

--- cypress/integration/examples/TestMarquee.js
+++ cypress/integration/examples/TestMarquee.js
@@ -3,6 +3,7 @@ describe('Marquee Click Test', () => {
     //Test For Selecting Different Pokemon.
     it('Testing Select', () => {
         cy.visit("http://localhost:3000/");
+        cy.wait(2000);
         cy.get(":nth-child(6) > img").click();
         cy.get(".heading > p").should('have.text', "You've chosen Charizard!");
       })
